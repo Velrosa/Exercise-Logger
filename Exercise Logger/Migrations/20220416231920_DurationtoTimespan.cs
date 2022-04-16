69e97127261848a5bc6e1b871ea8940c9985ac70
Velrosa <steezie21@hotmail.co.uk>
2022-04-17 00:22:00 +0100

    Added Models (Exercise and Context) Installed missing EntityFramework.Tools package. Added migrations, updated database, created tables.

---
+++ Exercise Logger/Migrations/20220416231920_DurationtoTimespan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Exercise_Logger.Migrations
+{
+    public partial class DurationtoTimespan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Duration",
+                table: "Exercise",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Duration",
+                table: "Exercise",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+    }
+}
---
+++ Exercise Logger/Models/ExerciseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Exercise_Logger.Models
+{
+
+    internal class ExerciseContext : DbContext
+    {
+        public DbSet<Exercise> Exercise { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Database=ExerciseDB;Integrated Security=True");
+        }
+    }
+}
