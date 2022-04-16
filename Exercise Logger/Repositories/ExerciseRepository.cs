5a7b60b26b48ca46a9cce9a1ed1c76f33968f171
Velrosa <steezie21@hotmail.co.uk>
2022-04-17 00:29:26 +0100

    Repository Implimentation added.

--- Exercise Logger/Repositories/ExerciseRepository.cs
+++ Exercise Logger/Repositories/ExerciseRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Exercise_Logger.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace Exercise_Logger
 {
-    internal class ExerciseRepository
+    internal class ExerciseRepository : IExerciseRepository
     {
+        private readonly ExerciseContext _context;
+
+        public ExerciseRepository(ExerciseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Exercise> Create(Exercise exercise)
+        {
+            _context.Exercise.Add(exercise);
+            await _context.SaveChangesAsync();
+            return exercise;
+        }
+
+        public async Task Delete(int id)
+        {
+            var exerciseToDelete = await _context.Exercise.FindAsync(id);
+            _context.Exercise.Remove(exerciseToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Exercise>> Get()
+        {
+            return await _context.Exercise.ToListAsync();
+        }
+
+        public async Task<Exercise> Get(int id)
+        {
+            return await _context.Exercise.FindAsync(id);
+        }
+
+        public async Task Update(Exercise exercise)
+        {
+            _context.Entry(exercise).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- Exercise Logger/Repositories/IExerciseRepository.cs
+++ Exercise Logger/Repositories/IExerciseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercise_Logger.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Exercise_Logger
 {
     internal interface IExerciseRepository
     {
+        Task<IEnumerable<Exercise>> Get();
+        Task<Exercise> Get(int id);
+        Task<Exercise> Create(Exercise exercise);
+        Task Update(Exercise exercise);
+        Task Delete(int id);
     }
 }
