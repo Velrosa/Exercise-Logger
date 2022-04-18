63a76af54563cbc416ec8084e1fadcba70e55752
Velrosa <steezie21@hotmail.co.uk>
2022-04-18 22:03:37 +0100

    Removed unused code snippet from Program.cs

--- Exercise Logger/Program.cs
+++ Exercise Logger/Program.cs
@@ -9,8 +9,6 @@ namespace Exercise_Logger
     {
         static async Task Main(string[] args)
         {
-
-            //UserInput userInput = new UserInput(new ExerciseService(new ExerciseController(new ExerciseRepository(new ExerciseContext()))));
             var container = ContainerConfig.Configure();
 
             while (true)
